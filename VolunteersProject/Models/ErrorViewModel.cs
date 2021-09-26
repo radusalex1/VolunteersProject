b514cdc9be2140dd430896e032d227ec335d46ca
radus_alexandru@yahoo.com <radus_alexandru@yahoo.com>
2021-09-26 12:16:21 +0300

    Add project files.

---
+++ VolunteersProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VolunteersProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
