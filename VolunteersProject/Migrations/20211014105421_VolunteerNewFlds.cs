0b3e625b59f0b5328b7d6e6adae5659d89df99c3
radus_alexandru@yahoo.com <radus_alexandru@yahoo.com>
2021-10-14 13:56:23 +0300

    Migration fixed

---
+++ VolunteersProject/Migrations/20211014105421_VolunteerNewFlds.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VolunteersProject.Migrations
+{
+    public partial class VolunteerNewFlds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DescriptionContributionToHub",
+                table: "Volunteers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FaceBookProfile",
+                table: "Volunteers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InstagramProfile",
+                table: "Volunteers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DescriptionContributionToHub",
+                table: "Volunteers");
+
+            migrationBuilder.DropColumn(
+                name: "FaceBookProfile",
+                table: "Volunteers");
+
+            migrationBuilder.DropColumn(
+                name: "InstagramProfile",
+                table: "Volunteers");
+        }
+    }
+}
