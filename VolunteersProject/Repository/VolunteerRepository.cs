bfb28c0fef5b994e8ebbeb2ec79d54051b8ac22c
Alexandru Ciprian <ciprian_alexandru@hotmail.com>
2021-11-01 07:20:06 +0200

    Implment GetAvailableVolunteers()

--- VolunteersProject/Repository/VolunteerRepository.cs
+++ VolunteersProject/Repository/VolunteerRepository.cs
@@ -6,40 +6,60 @@ using VolunteersProject.Models;
 
 namespace VolunteersProject.Repository
 {
+    /// <summary>
+    /// Repository for volunteers.
+    /// </summary>
     public class VolunteerRepository : IVolunteerRepository
     {
         private readonly VolunteersContext _context;
 
+        /// <summary>
+        /// Contructor
+        /// </summary>
+        /// <param name="context"></param>
         public VolunteerRepository(VolunteersContext context)
         {
             _context = context;
         }
 
-        public List<Volunteer> GetAvailableVolunteers(int ContributionID)
+        /// <summary>
+        /// Get all not assigned volunteers to the current contribution id.
+        /// </summary>
+        /// <param name="contributionId">Contribution id.</param>
+        /// <returns>List of not assigned volunteers.</returns>
+        public List<Volunteer> GetAvailableVolunteers(int contributionId)
         {
-            if(ContributionID==null)
-            {
-                return new List<Volunteer>();
-            }
             var volunteers = _context.Volunteers
-                .Include(e => e.Enrollments)
-                .ThenInclude(c => c.contribution)
-                .ToList();
+             .Include(e => e.Enrollments)
+             .ThenInclude(c => c.contribution)
+             .ToList();            
 
-            var volunteersFiltred = volunteers.Where(v => v.Enrollments.Any(c => c.contribution.ID!= ContributionID)).ToList();
+            var volunteersAssigned = volunteers.Where(v => v.Enrollments.Any(c => c.contribution.ID == contributionId));
 
-            return volunteersFiltred;
+            var volunteersAvailable = volunteers.Where(v => v.Enrollments.Any(c => c.contribution.ID != contributionId) && !volunteersAssigned.Contains(v)).ToList();
+
+            var volunteersWithNoAnyAssignments = _context.Volunteers.Where(v => v.Enrollments.Count == 0).ToList();
+
+            return volunteersAvailable.Union(volunteersWithNoAnyAssignments).ToList();
         }
 
+        /// <summary>
+        /// Get volunteer by id.
+        /// </summary>
+        /// <param name="id">Volunteer id.</param>
+        /// <returns></returns>
         public Volunteer GetVolunteerById(int id)
         {
             return _context.Volunteers.FirstOrDefault(i => i.ID.Equals(id));
         }
 
+        /// <summary>
+        /// Get all volunteers.
+        /// </summary>
+        /// <returns>List of all volunteers.</returns>
         public List<Volunteer> GetVolunteers()
-        {            
+        {
             return _context.Volunteers.ToList();
         }
-        
     }
 }
