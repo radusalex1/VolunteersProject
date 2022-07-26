3a9c9b8ecbd7c0cf4ac22e95fac3fa52c73c5d65
Ciprian Alexandru <ciprian.alexandru@accenture.com>
2022-07-26 13:32:28 +0300

    Add comments

--- VolunteersProject/Models/User.cs
+++ VolunteersProject/Models/User.cs
@@ -4,18 +4,33 @@ using System.Collections.Generic;
 namespace VolunteersProject.Models
 {
     /// <summary>
-    /// used to get logged user from dbo.
+    /// Used to manipulate logged user.
     /// </summary>
     public class User
     {
+        /// <summary>
+        /// Gets or sets id.
+        /// </summary>
         public int Id { get; set; }
-        //public string FirstName { get; set; }
-        //public string LastName { get; set; }
+                
+        /// <summary>
+        /// Gets or sets the username.
+        /// </summary>
         public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user role.
+        /// </summary>
         public Role Role { get; set; }
 
+        /// <summary>
+        /// Gets or sets assigned volunteers.
+        /// </summary>
         public ICollection<Volunteer> Volunteers { get; set; }
 
+        /// <summary>
+        /// Gets or sets password.
+        /// </summary>
         [JsonIgnore]
         public string Password { get; set; }
     }
