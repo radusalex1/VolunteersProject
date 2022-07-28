335459a3ff316ad6913db8ee1431994e691f3c9a
Alexandru Ciprian <ciprian_alexandru@hotmail.com>
2022-07-28 17:43:06 +0300

    Improve error handling

--- VolunteersProject/Models/ErrorViewModel.cs
+++ VolunteersProject/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace VolunteersProject.Models
 {
     /// <summary>
@@ -7,10 +5,65 @@ namespace VolunteersProject.Models
     /// </summary>
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        private int requestId;
+        private bool showRequestId;
+        private string errMsg;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="requestId">Request id.</param>
+        /// <param name="showRequestId">Show request id.</param>
+        /// <param name="errMsg">Error message.</param>
+        public ErrorViewModel(int requestId = 0, bool showRequestId = false, string errMsg="")
+        {
+            this.requestId = requestId;
+            this.showRequestId = showRequestId;
+            this.errMsg = errMsg;
+        }
 
-        public string ErrorMessage { get; internal set; }
+        /// <summary>
+        /// Gets or sets request id.
+        /// </summary>
+        public string RequestId {
+            get
+            {
+                return requestId.ToString();
+            }
+            set
+            {
+                _ = requestId;
+            }
+        }
+
+        /// <summary>
+        /// Get or sets show request id.
+        /// </summary>
+        public bool ShowRequestId
+        {
+            get
+            {
+                return showRequestId;
+            }
+            set
+            {
+                _ = requestId != 0 && showRequestId;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets error message.
+        /// </summary>
+        public string ErrorMessage
+        {
+            get
+            {
+                return errMsg;
+            }
+            set
+            {
+                _ = errMsg;
+            }
+        }
     }
 }
